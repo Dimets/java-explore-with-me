72b25f0109d9bd1537a04ffdb71af3263026e6d5
Dimets <dimets1977@gmail.com>
2022-11-12 20:22:21 +0300

    style: optimize imports

--- ewm-service/src/main/java/ru/practicum/explorewme/user/model/User.java
+++ ewm-service/src/main/java/ru/practicum/explorewme/user/model/User.java
@@ -3,10 +3,8 @@ package ru.practicum.explorewme.user.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.practicum.explorewme.event.model.Event;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Table(name = "users", schema = "public")
